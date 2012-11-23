79798f61d38d161e039efec9224a1c52b4f5179c
slunzok <slunzok@gmail.com>
2012-11-23 15:47:50 +0100

    Basic poll application (Django 1.4.2)

---
+++ mysite/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+#urlpatterns = patterns('polls.views',
+    #url(r'^polls/$', 'index'),
+    #url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+    #url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+    #url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+#)
+
+#urlpatterns += patterns('',
+    #url(r'^admin/', include(admin.site.urls)),
+#)
